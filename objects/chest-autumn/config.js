6554914b63882de45e839d88b0d2ac071a9d2c5b
Ryan Kubik <rkubik@twilio.com>
2021-04-07 16:58:53 -0700

    Make createChestConfig function to streamline chest creation

--- objects/chest-autumn/config.js
+++ objects/chest-autumn/config.js
@@ -1,32 +1,3 @@
-const objectiveCompletionResponder = require("../objectiveCompletionResponder/config");
+const createChestConfig = require("../../scripts/createChestConfig");
 
-module.exports = {
-  ...objectiveCompletionResponder,
-  animations: {
-    objectiveCompleted: {
-      frames: [1],
-      frameRate: 1,
-    },
-    objectiveCompleting: {
-      frames: [0, 1],
-      frameRate: 2,
-    },
-  },
-  spriteSheets: {
-    chestAutumn: {
-      fileName: "AutumnChest.png",
-      frameDimensions: {
-        width: 24,
-        height: 24,
-      },
-    },
-  },
-  properties: {
-    sprite: {
-      useGidAsDefaultFrameIndex: false,
-      defaultFrameIndex: 0,
-      spriteSheet: "chestAutumn",
-      layers: [],
-    },
-  },
-};
+module.exports = createChestConfig("chestAutumn", "AutumnChest.png");
--- objects/chest-red/config.js
+++ objects/chest-red/config.js
@@ -1,32 +1,3 @@
-const objectiveCompletionResponder = require("../objectiveCompletionResponder/config");
+const createChestConfig = require("../../scripts/createChestConfig");
 
-module.exports = {
-  ...objectiveCompletionResponder,
-  animations: {
-    objectiveCompleted: {
-      frames: [1],
-      frameRate: 1,
-    },
-    objectiveCompleting: {
-      frames: [0, 1],
-      frameRate: 2,
-    },
-  },
-  spriteSheets: {
-    chestRed: {
-      fileName: "chest-red.png",
-      frameDimensions: {
-        width: 24,
-        height: 24,
-      },
-    },
-  },
-  properties: {
-    sprite: {
-      useGidAsDefaultFrameIndex: false,
-      defaultFrameIndex: 0,
-      spriteSheet: "chestRed",
-      layers: [],
-    },
-  },
-};
+module.exports = createChestConfig("chestRed", "chest-red.png");
