0d88486f1044944778094cf466ec103b4bc5e37a
Ryan Kubik <rkubik@twilio.com>
2021-04-13 16:31:10 -0700

    Create verticalLaser object

--- objects/laser-vertical/config.js
+++ objects/laser-vertical/config.js
@@ -1,39 +1,10 @@
-const createObjectiveCompletionResponderConfig = require("../../scripts/objectiveCompletionResponder");
+const createLaserConfig = require("../../scripts/createLaserConfig");
 
-module.exports = {
-  ...createObjectiveCompletionResponderConfig({
-    renderObjectiveCompleted(self) {
-      console.log(self, self.sprite, self.sprite.body, self.sprite.body.enable);
-      if (self.sprite.body) {
-        self.sprite.body.enable = false;
-      }
-    },
-  }),
-  animations: {
-    objectiveCompleted: {
-      frames: [1],
-      frameRate: 1,
-    },
-    objectiveCompleting: {
-      frames: [0, 1],
-      frameRate: 2,
-    },
-  },
-  spriteSheets: {
-    laserVertical: {
-      fileName: "LaserBlockade_vertical.png",
-      frameDimensions: {
-        width: 24,
-        height: 24,
-      },
-    },
-  },
-  properties: {
-    sprite: {
-      useGidAsDefaultFrameIndex: false,
-      defaultFrameIndex: 0,
-      spriteSheet: "laserVertical",
-      layers: [],
-    },
-  },
-};
+module.exports = createLaserConfig(
+  "laserVertical",
+  "LaserBlockade_vertical.png",
+  {
+    width: 24,
+    height: 72,
+  }
+);
