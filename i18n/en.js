820d37c937314b3c6577480f50550c196e3bd9d0
rodrigostackbuilders <raparicio@stackbuilders.com>
2021-09-14 18:05:30 -0300

    i18n files

---
+++ i18n/en.js
@@ -0,0 +1,5 @@
+const keys = {
+  'menu.pressToStart': 'press spacebar to start',
+};
+
+module.exports = keys;
---
+++ i18n/es.js
@@ -0,0 +1,5 @@
+const keys = {
+  'menu.pressToStart': 'presione la barra espaciadora para comenzar',
+};
+
+module.exports = keys;
