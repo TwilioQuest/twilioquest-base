debf1adc1dc997353da6791ddaf92e872e62b4dc
rodrigostackbuilders <raparicio@stackbuilders.com>
2021-09-29 15:54:47 -0300

    using new approach in pug files

--- i18n/en.js
+++ i18n/en.js
@@ -13,4 +13,22 @@ module.exports = {
     Time to get started!<br/><br/> I might want to say hi to Cedric and the crew as well.
   </i>
   `,
+  'mica.dossier': `How did that get in there?! It's true, but my career was short and not 
+  all that successful. My most famous role was as an anthropomorphic fruit.
+  I performed a memorable song and dance number impressing upon children the 
+  importance of eating five servings of fruits and vegetables a day.<br/><br/>
+  So if you're ever on a mission where you need someone to pretend they're 
+  an apple, I've got you covered.`,
+  'mica.whatsThis': `That's the <span class="highlight">navigation map</span>. You can use the 
+  nav map to travel to a destination in The Cloud. We've highlighted a few 
+  hot spots on the map that we think could use your attention. You should 
+  visit these locations whenever you have time.`,
+  'mica.about': `Oh, sure - I'm Mica, your communications officer aboard the Fog Owl. In 
+  addition to being fluent in several languages, I can help handle 
+  sensitive negotiations or resolve conflicts with anyone we encounter 
+  while exploring The Cloud.`,
+  'mica.default': `Hey Operator! Where are we off to today?`,
+  'mica.dossierResponse': '[checks service records] I understand you were also a child actor?',
+  'mica.aboutResponse': 'Could you remind me who you are, and what you do on the ship?',
+  'mica.whatsThisResponse': 'What is that platform behind you?'
 };
--- i18n/es.js
+++ i18n/es.js
@@ -13,4 +13,22 @@ module.exports = {
     ¡Es hora de empezar!<br/><br/> Quizás también quiera saludar a Cedric y al equipo.
   </i>
   `,
+  'mica.dossier': `¡¿Cómo llegó eso ahí?! Es cierto, pero mi carrera fue corta y no con
+  todo ese éxito. Mi papel más famoso fue el de fruta antropomórfica.
+  Interpreté una canción memorable y un número de baile para impresionar
+  a los niños sobre la importancia de comer cinco porciones de frutas y verduras al día.<br/><br/>
+  Entonces, si alguna vez estás en una misión en la que necesitas a alguien que finja que es
+  una manzana, te tengo cubierto.`,
+  'mica.whatsThis': `Ese es el <span class="highlight">mapa de navegación</span>. Puedes usar el
+  mapa de navegación para viajar a un destino en La Nube. Hemos destacado algunos
+  puntos calientes en el mapa que creemos que podrían necesitar su atención. Debería
+  visitar estos lugares siempre que tenga tiempo.`,
+  // 'mica.about': `Oh, claro, soy Mica, tu oficial de comunicaciones a bordo del Búho de Niebla.
+  // Además de dominar varios idiomas, puedo ayudar a manejar
+  // negociaciones delicadas o resolver conflictos con cualquier persona que encontremos
+  // mientras explora La Nube.`,
+  'mica.default': `¡Hola operador! ¿A dónde vamos hoy?`,
+  'mica.dossierResponse': '[revisa los registros de servicio] Tengo entendido que usted también era un actor infantil.',
+  'mica.aboutResponse': '¿Podrías recordarme quién eres y qué haces en el barco?',
+  'mica.whatsThisResponse': '¿Cuál es esa plataforma detrás de ti?'
 };
