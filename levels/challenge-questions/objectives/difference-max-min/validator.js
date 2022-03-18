a3703ce3e8d4e2d2a6f1f46cb5ffdd9f7f5eb88e
Ryan Kubik <rkubik@twilio.com>
2022-03-18 16:17:03 -0700

    Write test cases for difference-max-min

--- levels/challenge-questions/objectives/difference-max-min/validator.js
+++ levels/challenge-questions/objectives/difference-max-min/validator.js
@@ -1,10 +1,39 @@
 const assert = require("assert");
 
+const assertTestCase = (testFunction) => (input, expected) => {
+  const testResult = testFunction(input);
+
+  assert.strictEqual(
+    testResult,
+    expected,
+    `Expected "${expected}" from input "${input}", but received "${testResult}".`
+  );
+};
+
 module.exports = async function (helper) {
-  // test harness
-  // look up code in the player's quest ide
-  // eval quest ide code
-  // run assertions on it
-  console.log(helper.world);
-  console.log(helper.world.getQuestIdeUserCode("difference-max-min"));
+  let context;
+
+  try {
+    context = await helper.pullVarsFromQuestIdeUserCodeLocalScope(
+      ["differenceMinMax"],
+      "difference-max-min"
+    );
+
+    assert(
+      context.differenceMinMax,
+      "The function differenceMinMax is not defined!"
+    );
+
+    const test = assertTestCase(context.differenceMinMax);
+
+    test([1, 2, 3, 4, 5], 4);
+    test([100, 0], 100);
+    test([3, 5, -2, 5], 7);
+    test([8, 1, 5, 9], 8);
+  } catch (err) {
+    helper.fail(err);
+    return;
+  }
+
+  helper.success("You did it!");
 };
