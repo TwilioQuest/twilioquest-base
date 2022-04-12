ecaf4bb9711ba708bdf44757a0000a855122d8d3
kerrielake <57815167+kerrielake@users.noreply.github.com>
2022-04-12 15:20:10 -0400

    Dev Fundamentals Assets
    Added:
    -Tileset_Dev-Fundamentals-Exterior
    -Object "door-dev-tower"
    -Object "tower-piece" x10

---
+++ objects/tower-piece-6/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  animations: {
+    idle: {
+      frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      frameRate: 8,
+    },
+  },
+  spriteSheets: {
+    twilioquestTowerPiece6: {
+      fileName: "floatingtowerpiece6.png",
+      frameDimensions: {
+        width: 72,
+        height: 48,
+      },
+    },
+  },
+  properties: {
+    sprite: {
+      defaultFrameIndex: 0,
+      spriteSheet: "twilioquestTowerPiece6",
+      layers: [],
+    },
+    idleAnimations: {
+      animations: { idle: 100 },
+      minIdleTime: 0,
+      maxIdleTime: 0,
+    },
+  },
+};
