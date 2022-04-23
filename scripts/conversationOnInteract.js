04141fe2815f3d97d484d36151f10c4de1a6f47e
Ryan Kubik <rkubik@twilio.com>
2022-04-22 17:02:54 -0700

    Implement groundskeeper and ghost professor

---
+++ scripts/conversationOnInteract.js
@@ -0,0 +1,32 @@
+module.exports = function (self, event, world) {
+  if (self !== event.target) {
+    // This isn't our conversationalist
+    return;
+  }
+
+  if (self.observation) {
+    // The "observation" property can be set to have the Operator
+    // make an observation about a character without kicking off a convo
+    world.showNotification(self.observation);
+    return;
+  }
+
+  // Default to conversation and ensure appropriate values are set
+  const conversationName = self.conversation;
+  const conversationAvatar = self.conversationAvatar;
+
+  if (!conversationName) {
+    console.error(`"conversationName" property not set on interaction target.`);
+    return;
+  }
+
+  if (!conversationAvatar) {
+    console.error(
+      `"conversationAvatar" property not set on interaction target.`
+    );
+    return;
+  }
+
+  // Kick off conversation
+  world.startConversation(conversationName, conversationAvatar);
+};
