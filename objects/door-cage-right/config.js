c834b42f25f277a7b5ab028c7fdef6947293e509
Ryan Kubik <rkubik@twilio.com>
2022-04-25 15:29:57 -0700

    Create cage door left and right

---
+++ objects/door-cage-right/config.js
@@ -0,0 +1,40 @@
+const initOpenCloseFsm = require("../../scripts/initOpenCloseFsm");
+
+module.exports = {
+  state: {
+    fsm: null,
+  },
+  spriteSheets: {
+    twilioquestCageDoorRight: {
+      fileName: "CageDoor.png",
+      frameDimensions: {
+        width: 24,
+        height: 48,
+      },
+    },
+  },
+  animations: {
+    open: { frames: [4], frameRate: 1 },
+    opening: {
+      frames: [0, 1, 2, 3, 4],
+      frameRate: 6,
+    },
+    closed: { frames: [9], frameRate: 1 },
+    closing: {
+      frames: [5, 6, 7, 8, 9],
+      frameRate: 6,
+    },
+  },
+  properties: {
+    sprite: {
+      defaultFrameIndex: 0,
+      spriteSheet: "twilioquestCageDoorRight",
+      layers: [],
+    },
+  },
+  events: {
+    onMapDidLoad: initOpenCloseFsm({
+      smoothTransitionBetweenOpeningAndClosing: false,
+    }),
+  },
+};
