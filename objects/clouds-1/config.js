bbdddac7db295c630a994f05191751d3576ec317
Ryan Kubik <rkubik@twilio.com>
2022-05-27 12:35:09 -0700

    Add clouds-1 and 2 configs

---
+++ objects/clouds-1/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  animations: {},
+  spriteSheets: {
+    tqDevFundTowerClouds1: {
+      fileName: "Clouds1.png",
+      frameDimensions: {
+        width: 528,
+        height: 216,
+      },
+    },
+  },
+  properties: {
+    sprite: {
+      defaultFrameIndex: 0,
+      spriteSheet: "tqDevFundTowerClouds1",
+      layers: [],
+    },
+  },
+};
---
+++ objects/clouds-2/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  animations: {},
+  spriteSheets: {
+    tqDevFundTowerClouds2: {
+      fileName: "Clouds2.png",
+      frameDimensions: {
+        width: 528,
+        height: 216,
+      },
+    },
+  },
+  properties: {
+    sprite: {
+      defaultFrameIndex: 0,
+      spriteSheet: "tqDevFundTowerClouds2",
+      layers: [],
+    },
+  },
+};
