73e2236ee6b552e995da8b7345c2d09a3d6cbab2
Marvel Johnson <marvjohnson@twilio.com>
2022-06-09 16:14:34 -0700

    added check-for-palindrome mission to Ducktypium Forge

---
+++ levels/challenge-questions/objectives/check-for-palindrome/example.js
@@ -0,0 +1,15 @@
+function isPalindrome(array) {
+    // TODO: check for palindrome!
+  }
+
+  console.log("Test case 1:");
+  console.log(isPalindrome("tacocat"));
+  
+  console.log("Test case 2:");
+  console.log(isPalindrome(""));
+  
+  console.log("Test case 3:");
+  console.log(isPalindrome("heLLo"));
+  
+  console.log("Test case 4:");
+  console.log(isPalindrome("Never odd or even"));
