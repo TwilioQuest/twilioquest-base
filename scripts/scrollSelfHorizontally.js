1e28355ff935220b19a011465aa21026117e6102
Ryan Kubik <rkubik@twilio.com>
2022-06-10 15:20:17 -0700

    Disable wisp bodies, fix clouds ltr and rtl scrolling

--- scripts/scrollSelfHorizontally.js
+++ scripts/scrollSelfHorizontally.js
@@ -1,17 +1,26 @@
+const DIRECTION_LEFT_TO_RIGHT = "DIRECTION_LEFT_TO_RIGHT";
+const DIRECTION_RIGHT_TO_LEFT = "DIRECTION_RIGHT_TO_LEFT";
+
 module.exports = function scrollSelfHorizontally(self, world, speed) {
   const { widthInPixels } = world.__internals.level.map;
 
+  const scrollDirection =
+    speed > 0 ? DIRECTION_LEFT_TO_RIGHT : DIRECTION_RIGHT_TO_LEFT;
   self.sprite.body.velocity.x += speed;
 
   self.sprite.update = () => {
     const { body } = self.sprite;
 
     const leftEdge = body.x;
-    const rightEdge = body.x + body.width;
-
-    if (leftEdge > widthInPixels) {
+    if (
+      scrollDirection === DIRECTION_LEFT_TO_RIGHT &&
+      leftEdge > widthInPixels
+    ) {
       body.x = -body.width;
-    } else if (rightEdge < 0) {
+    }
+
+    const rightEdge = body.x + body.width;
+    if (scrollDirection === DIRECTION_RIGHT_TO_LEFT && rightEdge < 0) {
       body.x = widthInPixels;
     }
   };
