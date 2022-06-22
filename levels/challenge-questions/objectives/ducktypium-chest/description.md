c1fb618b194795cee63f09a163e6033f1fd615ba
Marvel Johnson <marvjohnson@twilio.com>
2022-06-22 15:26:12 -0700

    Added ducktypium-chest objective

---
+++ levels/challenge-questions/objectives/ducktypium-chest/description.md
@@ -0,0 +1,20 @@
+# Help Cedric Get Past the Door
+
+<div class="aside">
+<h3>Requirements</h3>
+<ul>
+  <li>Enter the action you’d like Cedric to perform in an attempt to get past the door.</li>
+  <li>Once you're done, press <em>HACK</em>.</li>
+</ul>
+</div>
+
+Cedric has been exploring a temple in search for the Dark Ducktypium Chest, but hasn’t had any luck. His O.W.L.(Object Waypoint Locator) unit has led him to this room, with a door impeding any further progress. There’s a lever that when pulled, opens the path. The only issue is that the lever rises again if he lets it go, causing the door to quickly slam shut. What should Cedric do to overcome this obstacle? The valid instructions are `race the lever`, `break barrels and pots`, `give up`, and `kick the door`. Each of these will tell Cedric to perform an action.
+
+For example, to tell Cedric to try and kick the door down you would enter the string: `kick the door`.
+
+
+<% const imagePath = await resolveAbsolutePath("images/challenge-questions/turtle2_x4.png") %>
+
+![Room that Cedric is in](<%= imagePath %>)
+
+Once you've written out Cedric's instructions in the input on the right, click the _HACK_ button!
---
+++ levels/challenge-questions/objectives/ducktypium-chest/validator.js
@@ -0,0 +1,53 @@
+module.exports = async function (helper) {
+  try {
+    const action = helper.getNormalizedInput("action");
+    const correctAction = "break barrels and pots";
+    const validActions = ["race the lever", correctAction, "give up", "kick the door"];
+
+    if (action.length === 0) {
+      helper.fail(
+        "There does not seem to be an action here! There should be a single string. Something like: 'kick the door'."
+      );
+      return;
+    }
+
+    if (
+      !validActions.some((action) => action === action)
+    ) {
+      // Check if any player action is invalid
+      helper.fail(
+        `The action you entered is not valid! The only valid actions are: ${validActions.join(
+          " "
+        )}.`
+      );
+      return;
+    }
+
+    if (action !== correctAction) {
+
+      // This is an invalid action
+      let message =
+        "Your action for Cedric did not get him through the door!";
+
+      switch(action) {
+        case 'kick the door':
+          message += ' Kicking the door didn\'t seem to do anything.';
+          break;
+        case 'give up':
+          message += ' He gave up for now. Maybe he\'ll try again later?';
+          break;
+        case 'race the lever':
+          message += ' He just wasn\'t fast enough to make it. There must be another way!';
+          break;
+      }
+
+      helper.fail(message);
+      return;
+    }
+  } catch (err) {
+    helper.fail(err);
+    return;
+  }
+
+  helper.success("You did it!");
+};
