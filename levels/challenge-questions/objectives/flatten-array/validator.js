8c861681aeeda802f13d1e32162f656c573c8f04
Marvel Johnson <marvjohnson@twilio.com>
2022-08-01 12:47:27 -0700

    Fixed flatten-array Objective Validator
    -Updated array equality checks
    -Added comments

--- levels/challenge-questions/objectives/flatten-array/validator.js
+++ levels/challenge-questions/objectives/flatten-array/validator.js
@@ -1,13 +1,41 @@
 const assert = require("assert");
 
+const wrapInBracketsIfArray = (value) =>
+  Array.isArray(value) ? `[${value}]` : String(value);
+
+const ARRAY_ELEMENT_DIFFERENT_ERROR_MESSAGE =
+  "One or more array elements are different or have different types.";
+const DEFAULT_ERROR_MESSAGE = (expected, input, testResult) => {
+  const expectedFormatted = wrapInBracketsIfArray(expected);
+  const inputFormatted = wrapInBracketsIfArray(input);
+  const testResultFormatted = wrapInBracketsIfArray(testResult);
+
+  return `Expected "${expectedFormatted}" from input "${inputFormatted}", but received "${testResultFormatted}".`;
+};
+
 const assertTestCase = (testFunction) => (input, expected) => {
   const testResult = testFunction(input);
 
-  assert.deepStrictEqual(
-    testResult,
-    expected,
-    `Expected "${expected}" from input "[${input}]", but received "${testResult}".`
+  // Checks for 'value' equality. This should only be true if both are 'Pure Ducktypium!'
+  const areEqualStrings = Object.is(
+    String(testResult).toLowerCase(),
+    String(expected).toLowerCase()
   );
+
+  // Checks for equal types, lengths, and element values
+  const areEqualArrays =
+    Array.isArray(expected) &&
+    Array.isArray(testResult) &&
+    testResult.length === expected.length &&
+    expected.every((element, idx) => {
+      if (!Object.is(element, testResult[idx]))
+        assert.fail(ARRAY_ELEMENT_DIFFERENT_ERROR_MESSAGE);
+
+      return true;
+    });
+
+  if (!areEqualStrings && !areEqualArrays)
+    assert.fail(DEFAULT_ERROR_MESSAGE(expected, input, testResult));
 };
 
 module.exports = async function (helper) {
@@ -19,17 +47,17 @@ module.exports = async function (helper) {
       "flatten-array"
     );
 
-    assert(
-      context.flattenArray,
-      "The function flattenArray is not defined!"
-    );
+    assert(context.flattenArray, "The function flattenArray is not defined!");
 
     const test = assertTestCase(context.flattenArray);
 
     test([], "Pure Ducktypium!");
     test([1, "two", 3, "four"], [1, "two", 3, "four"]);
     test([1, 3, 3, 7, ["legacy wuz here"]], [1, 3, 3, 7, "legacy wuz here"]);
-    test(["python", ["javascript", ["api", ["messaging"]]]], ["python", "javascript", "api", "messaging"]);
+    test(
+      ["python", ["javascript", ["api", ["messaging"]]]],
+      ["python", "javascript", "api", "messaging"]
+    );
   } catch (err) {
     helper.fail(err);
     return;
