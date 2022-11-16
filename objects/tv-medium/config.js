258a30d838fb5e1c57df376eda0eb86baa91b4d4
Ryan Kubik <rkubik@twilio.com>
2022-11-15 17:12:38 -0800

    Fix lovelace sparkle and add medium_tv config

---
+++ objects/tv-medium/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  animations: {},
+  spriteSheets: {
+    tqTvMedium: {
+      fileName: "TV_Medium.png",
+      frameDimensions: {
+        width: 48,
+        height: 72,
+      },
+    },
+  },
+  properties: {
+    sprite: {
+      defaultFrameIndex: 0,
+      spriteSheet: "tqTvMedium",
+      layers: [],
+    },
+  },
+};
